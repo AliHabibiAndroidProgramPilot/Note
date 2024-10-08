e05c074853b87f122b4f9c3bb947dfef4fdd91c8
Ali <biz.alihabibi@gmail.com>
2024-10-08 23:46:32 +0330

    Set ToolBar As Action Bar & Manage Back Button Pressed

--- app/src/main/java/com/ir/ali/note/NoteActivity.kt
+++ app/src/main/java/com/ir/ali/note/NoteActivity.kt
@@ -1,8 +1,8 @@
 package com.ir.ali.note
 
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.ir.ali.note.databinding.ActivityNoteBinding
 
@@ -12,8 +12,28 @@ class NoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         val titleEditText = binding.edtNoteTitle
         titleEditText.requestFocus()
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+    }
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith(
+            "super.onBackPressed()",
+            "androidx.appcompat.app.AppCompatActivity"
+        )
+    )
+    override fun onBackPressed() {
+        Log.i("isWorking", "yes Working")
+        super.onBackPressed()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.i("isWorking", "yes Working")
+        onBackPressed()
+        return true
     }
 }
