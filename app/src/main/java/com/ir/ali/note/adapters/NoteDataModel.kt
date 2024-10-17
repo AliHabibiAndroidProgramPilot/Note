712fa7ba123a3778ef1adb74d8ccb8747637c36e
Ali <biz.alihabibi@gmail.com>
2024-10-17 16:32:25 +0330

    Implement Selecting Notes In DAO

--- app/src/main/java/com/ir/ali/note/adapters/NoteDataModel.kt
+++ app/src/main/java/com/ir/ali/note/adapters/NoteDataModel.kt
@@ -1,6 +1,7 @@
 package com.ir.ali.note.adapters
 
 data class NoteDataModel(
+     var noteId: Int,
      var noteTitle: String,
      var noteText: String,
      var noteDate: String
--- app/src/main/java/com/ir/ali/note/database/databasedao/NoteDAO.kt
+++ app/src/main/java/com/ir/ali/note/database/databasedao/NoteDAO.kt
@@ -1,13 +1,15 @@
 package com.ir.ali.note.database.databasedao
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.util.Log
+import com.ir.ali.note.adapters.NoteDataModel
 import com.ir.ali.note.database.DataBaseHelper
 import com.ir.ali.note.database.notedatamodel.NotesDataModel
 
-class NoteDAO(
-    private val dataBase: DataBaseHelper
-) {
+class NoteDAO(private val dataBase: DataBaseHelper) {
     private val contentValues = ContentValues()
+    private lateinit var cursor: Cursor
 
     fun insertNote(note: NotesDataModel): Boolean {
         val accessDataBase = dataBase.writableDatabase
@@ -17,6 +19,48 @@ class NoteDAO(
         accessDataBase.close()
         return insertResult > 0
     }
+
+    fun getNotes(deleted: String, archived: String): ArrayList<NoteDataModel> {
+        val accessDataBase = dataBase.readableDatabase
+        val sqlQuery: String =
+            "SELECT ${DataBaseHelper.NOTES_ID}, ${DataBaseHelper.NOTES_TITLE}, ${DataBaseHelper.NOTES_TEXT} " +
+                    "FROM ${DataBaseHelper.NOTES_TABLE} " +
+                    "WHERE ${DataBaseHelper.NOTES_DELETE_STATE} = ? " +
+                    "OR ${DataBaseHelper.NOTES_ARCHIVE_STATE} = ?"
+        cursor = accessDataBase.rawQuery(
+            sqlQuery,
+            arrayOf(deleted, archived)
+        )
+        val data = getDataFromCursor()
+        cursor.close()
+        accessDataBase.close()
+        return data
+    }
+
+    private fun getDataFromCursor(): ArrayList<NoteDataModel> {
+        val tempData = ArrayList<NoteDataModel>()
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    val id =
+                        cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseHelper.NOTES_ID))
+                    val title =
+                        cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.NOTES_TITLE))
+                    val text =
+                        cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.NOTES_TEXT))
+                    val date =
+                        cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.NOTES_DATE))
+                    tempData.add(
+                        NoteDataModel(id, title, text, date)
+                    )
+                } while (cursor.moveToNext())
+            }
+        } catch (e: IllegalArgumentException) {
+            Log.e("IllegalArgumentException", e.cause.toString())
+        }
+        return tempData
+    }
+
     private fun setContentValues(note: NotesDataModel) {
         contentValues.clear()
         contentValues.put(DataBaseHelper.NOTES_TITLE, note.noteTitle)
